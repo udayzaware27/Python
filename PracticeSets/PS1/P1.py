154fc0e70021ee8b99676f5656f38a57abea0e4c
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-12 22:58:03 +0530

    Problem 1

---
+++ PracticeSets/PS1/P1.py
@@ -0,0 +1,8 @@
+# 1. Print Twinkle Twinkle Little Star Poem
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are!''')
