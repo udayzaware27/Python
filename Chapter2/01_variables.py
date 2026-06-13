c8ea9b174da100d045a788b0b4e2e91f1eaed27e
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-13 09:43:57 +0530

    Variables in Python

---
+++ Chapter2/01_variables.py
@@ -0,0 +1,10 @@
+''' Rules for define variable 
+1. can contain alphabets, digits and underscore
+2. can only starts with alphabet and underscore
+3. can not start with a digit or any special character
+4. no white space allowed 
+'''
+
+a = 3
+b = 4
+print(a+b)
