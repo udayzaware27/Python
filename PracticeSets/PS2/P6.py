26081864fffb4305837a9650f7e22c3bec57ce92
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-14 18:25:25 +0530

    problem 6

---
+++ PracticeSets/PS2/P6.py
@@ -0,0 +1,5 @@
+# Write a Python program to calculate the square of a number entered by the user.
+
+a = int(input("Enter value of a: "))
+
+print("Square is: ",a**2) #use after a variable '**' for take a power
