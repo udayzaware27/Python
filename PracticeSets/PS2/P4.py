425c74fce186d9d064053fe570de22de55556d27
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-14 18:14:16 +0530

    problem 4

---
+++ PracticeSets/PS2/P4.py
@@ -0,0 +1,10 @@
+'''
+Use a comparison operator to find out whether a given variable a is greater than b or not.
+Take a = 34
+Take b = 80
+'''
+
+a = 34
+b = 80 
+
+print("a is greater than b: ", a>b)
