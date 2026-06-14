9a707d3854e9ec897b6691aa6d046bbb0c71e34b
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-14 22:14:24 +0530

    Slicing with skip value

--- Chapter3/01_into_string.py
+++ Chapter3/01_into_string.py
@@ -10,3 +10,8 @@ print(c[0:]) # same as c[0:5] means to the length
 # Negative Slicing
 print(c[-4 : -1]) #start from -4 till -1 (excluding -1) also same as c[1:4]
 print(c[1:4])
+
+
+# Slicing with skip value
+k = "amazing"
+print(k[1:6:3])  # it first execute k[1:6], read first letter then skip 3 letters and read next
