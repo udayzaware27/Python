d9da9c6a2cb6b01ddd35216c76c27b9c87319f01
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-14 23:00:29 +0530

    Escape sequence character

---
+++ Chapter3/03_escape_seq.py
@@ -0,0 +1,11 @@
+# escape sequence character
+
+a = "India is my\\country\nand it\tis \"beautiful\""
+
+        # `\n` --> New line        
+        # `\t` --> Tab space       
+        # `\'` --> Single quote    
+        # `\"` --> Double quote    
+        # `\\` --> Backslash       
+
+print(a)
