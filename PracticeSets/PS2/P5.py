9b0b5cf6ab14a94139acd21e7da1068c1cd5afcc
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-14 18:19:58 +0530

    Problem 5

---
+++ PracticeSets/PS2/P5.py
@@ -0,0 +1,7 @@
+# Write a Python program to find the average of two numbers entered by the user.
+
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+c = (a+b)/2
+print("Average of a & b is: ", c)
