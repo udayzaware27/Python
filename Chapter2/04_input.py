d2c0015c25ead818744cc0a42dec2b2b82684888
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-14 17:57:39 +0530

    Input Function

---
+++ Chapter2/04_input.py
@@ -0,0 +1,8 @@
+a = int(input("enter number 1: ")) #input --> to take input from user
+
+b = int(input("enter number 2: ")) #int --> to convert input into integer
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+
+print("Sum is: ", a+b)
