a86127a3aa098c92d77303637909db1513edab69
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-15 22:05:07 +0530

    Tuple in python

---
+++ Chapter4/03_tuple.py
@@ -0,0 +1,12 @@
+t = (1, 44, False, "orange", 3.14, 44) # tuple is a immutable
+
+print(t)
+print(type(t)) # it will return class <tuple>
+
+print(t[2])
+print(t[1:4])
+
+print(t.count(44)) #return number of occurances
+
+print(t.index("orange")) #return index of element
+
