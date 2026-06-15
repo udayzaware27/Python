062062a92dbb33bf213d811e254fd366be16923a
Uday Zaware <udayzaaware@Udays-MacBook-Air.local>
2026-06-15 09:04:23 +0530

    Problem 1

---
+++ PracticeSets/PS3/p1..py
@@ -0,0 +1,13 @@
+''' Write a Python program to display a user-entered name followed by "Good Afternoon" 
+ using the input() function.'''
+
+name = input("Enter your name: ")
+
+print("Good Afternoon",name) #general way
+
+
+''' f-string (formatted string) is a convinient way to insert a variable or expression 
+directly inside string '''
+
+print(f"Good Morning {name}") 
+
