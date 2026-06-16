fcf7494dbb8ab7439c766f43834d7e0ffa6df195
Uday Zaware <udayzaware09@gmail.com>
2026-06-16 08:28:53 +0530

    Problem 1

---
+++ PracticeSets/PS4/p1.py
@@ -0,0 +1,27 @@
+#Write a Python program to store seven fruits in a list entered by the user.
+
+#solution1
+fruits = []
+
+f1 = input("Enter Fruit1 name: ")
+fruits.append(f1)
+f2 = input("Enter Fruit2 name: ")
+fruits.append(f2)
+f3 = input("Enter Fruit3 name: ")
+fruits.append(f3)
+f4 = input("Enter Fruit4 name: ")
+fruits.append(f4)
+f5 = input("Enter Fruit5 name: ")
+fruits.append(f5)
+f6 = input("Enter Fruit6 name: ")
+fruits.append(f6)
+f7 = input("Enter Fruit7 name: ")
+fruits.append(f7)
+
+print(fruits)
+
+
+#solution2
+
+l1 = [f1, f2, f3, f4, f5, f6, f7]
+print(l1)
